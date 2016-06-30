e21e6775bec70b86f2f29154fd85aa8bb5a7952c
oharsta <oharsta@zilverline.com>
2016-06-30 09:51:40 +0200

    Forgot new file to check in

---
+++ src/main/java/idensys/saml/CustomSAMLBootstrap.java
@@ -0,0 +1,21 @@
+package idensys.saml;
+
+import org.opensaml.Configuration;
+import org.opensaml.xml.security.BasicSecurityConfiguration;
+import org.opensaml.xml.signature.SignatureConstants;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class CustomSAMLBootstrap extends org.springframework.security.saml.SAMLBootstrap {
+
+  public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    super.postProcessBeanFactory(beanFactory);
+
+    BasicSecurityConfiguration config = (BasicSecurityConfiguration) Configuration.getGlobalSecurityConfiguration();
+    config.registerSignatureAlgorithmURI("RSA", SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA256);
+    config.setSignatureReferenceDigestMethod(SignatureConstants.ALGO_ID_DIGEST_SHA256);
+
+  }
+
+}
